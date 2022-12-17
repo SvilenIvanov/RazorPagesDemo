386db9bee287335d2092da75b495e6e0e5a6a4cf
Svilen <svilen.ivanov01@gmail.com>
2022-12-17 14:12:03 +0200

    Created ApplicationDbContext and added the dbContext service

---
+++ RazorPagesProject/RazorPagesProject/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+using RazorPagesProject.Model;
+
+namespace RazorPagesProject.Data {
+    public class ApplicationDbContext : DbContext{
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) {
+
+        }
+        DbSet<Category> Category { get; set; }
+
+    }
+}
