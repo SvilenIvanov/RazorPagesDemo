12844e37337279a1bbc123bf3cdb6cbda24edc69
Svilen <svilen.ivanov01@gmail.com>
2022-12-23 00:34:13 +0200

    Added Edit category backend logic.
    Created 'back to list' button for create category and edit category

---
+++ RazorPagesProject/RazorPagesProject/Pages/Categories/Edit.cshtml.cs
@@ -0,0 +1,40 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using RazorPagesProject.Data;
+using RazorPagesProject.Model;
+
+namespace RazorPagesProject.Pages.Categories
+{
+    //[BindProperties]
+    public class EditModel : PageModel
+    {
+        [BindProperty]
+        public Category Category { get; set; }
+        private readonly ApplicationDbContext _db;
+
+        public EditModel(ApplicationDbContext db) {
+            _db = db;
+        }
+        
+        public void OnGet(int id)
+        {
+            Category = _db.Category.SingleOrDefault(i => i.Id == id);
+            //Category = _db.Category.Find(id);
+
+        }
+
+        public async Task<IActionResult> OnPost() {
+            if (Category.Name == Category.DisplayOrder.ToString()) {
+                ModelState.AddModelError("Category.Name", "The category name cannot be the same as the display order.");
+            }
+            if (ModelState.IsValid) {
+                _db.Update(Category);
+                _db.SaveChanges();
+                return RedirectToPage("Index");
+            }
+            return Page();
+        }
+    }
+}
